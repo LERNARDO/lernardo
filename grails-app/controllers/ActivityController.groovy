997e1ec67cd60a405670b763695e17c821985e4a
Allan Berger <mail@allanberger.com>
2009-09-25 14:50:38 +0200

    added month parameter to activity controller

--- grails-app/controllers/ActivityController.groovy
+++ grails-app/controllers/ActivityController.groovy
@@ -6,7 +6,8 @@ class ActivityController {
     def list = {
         params.offset = params.offset ? params.offset.toInteger(): 0
         params.max = params.max ? params.max.toInteger(): 10
-        def res = ['activityList': activityDataService.getActivities (params.offset, params.max),
+        params.perMonth = params.perMonth ? params.perMonth.toInteger(): 0
+        def res = ['activityList': activityDataService.getActivities (params.offset, params.max, params.perMonth),
                    'activityCount': activityDataService.getActivityCount ()]
         render (view:"list", model:res)
         
