f97635363831b787df7be4c9f3f9a334aee31d03
Alexander Zeillinger <aaz@lkult.at>
2009-11-13 13:53:04 +0100

    now using MetaDataService

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,155 +2,93 @@ import de.uenterprise.ep.EntitySuperType
 import de.uenterprise.ep.EntityType
 import de.uenterprise.ep.Entity
 import de.uenterprise.ep.Role
-import org.grails.plugins.jquery.calendar.domain.CalendarEventType
-import org.grails.plugins.jquery.calendar.domain.CalendarEvent
+import org.joda.time.DateMidnight
+import de.uenterprise.ep.profiles.PersonProfile
 
 class BootStrap {
-    def profileDataService
-    def templateDataService
-    def activityDataService
-    def articleDataService
-    def defaultObjectService
-    def entityHelperService
-    def sessionFactory
-    def calendarService
+  def profileDataService
+  def templateDataService
+  def activityDataService
+  def articleDataService
+  def defaultObjectService
+  def entityHelperService
+  def calendarDataService
+  def sessionFactory
 
-    def init = {servletContext->
-        profileDataService.init()
-        templateDataService.init()
-        activityDataService.init()
-        articleDataService.init()
+  def metaDataService
 
-        println "### Starting UeDomains ###"
-        initUeDomains()
-        println "### Finished UeDomains ###"
-        
-        def eventTypes = ['conference', 'contest', 'exam', 'meeting']
-        addEventTypes(eventTypes)
-        addEvents(eventTypes)
+  def init = {servletContext ->
+    defaultObjectService.onEmptyDatabase {
+      metaDataService.initialize()
+      createDefaultUsers()
+      //profileDataService.init()
+      templateDataService.init()
+      activityDataService.init()
+      articleDataService.init()
+      calendarDataService.init()
+    }
+    //initUeDomains()
 
-        calendarService.securityDelegate = [
-           getCurrentUser: {-> [id: 1]},
-           isUserAllowedToCreateEvents: {-> true},
-           isUserAllowedToEditEvent: {e -> true},
-           isUserAllowedToCreateReminder: {e -> true}
-       ]
+  }
 
-        calendarService.mailDelegate = [
-            sendReminder: {reminder ->  println 'reminder sent' },
-        ]
+  def destroy = {
+  }
+
+  void createDefaultUsers() {
+    EntityType etUser = metaDataService.etUser
+
+    // an admin user
+    entityHelperService.createEntityWithUserAndProfile("admin", etUser, "admin@ue.de", "Mr. Admin") {Entity ent ->
+      ent.user.addToAuthorities(metaDataService.adminRole)
+      ent.profile.tagline = "to be on top is our job"
+      ent.profile.gender = 1
+      ent.profile.location = "hier"
     }
 
-    def destroy = {
+    // a mod user
+    entityHelperService.createEntityWithUserAndProfile("alex", etUser, "aaz@lernardo.at", "Alexander Zeillinger") {Entity ent ->
+      ent.user.addToAuthorities(metaDataService.modRole)
+      ent.profile.tagline = "Simplicity is the ultimate sophistication"
+      ent.profile.gender = 1
     }
 
-    def addEventTypes(eventTypes) {
-        if (!CalendarEventType.count()) {
-            eventTypes.each {
-                new CalendarEventType(name: it).save(failOnError: true)
-            }
-        }
+    // some regular users
+    entityHelperService.createEntityWithUserAndProfile("patrizia", etUser, "pcr@lernardo.at", "Patrizia Rosenkranz") {Entity ent ->
+      PersonProfile prf = ent.profile
+
+      prf.birthday = new DateMidnight(1983, 07, 20).toDate()
+      prf.gender = 2
+      prf.profession = "Soziologin"
+      prf.location = "Berndorf"
+      prf.status = "workaholic"
+      prf.hobbies = "musik, tanz"
+      prf.webSite = "www.lkult.at"
+      prf.description = "Ist eine LKULT Mitarbeiterin."
     }
 
-    def addEvents(eventTypes) {
-        def DAYS = 20
-        Calendar day = GregorianCalendar.instance
-        day.set Calendar.MILLISECOND, 0
-        day.set Calendar.SECOND, 0
-        day.set Calendar.MINUTE, 0
-        day.roll Calendar.DAY_OF_YEAR, -DAYS/2 as int
-
-        // Events get added here
-        DAYS.times {
-            day.set Calendar.HOUR_OF_DAY, 14
-            def startDate = day.time
-            day.set Calendar.HOUR_OF_DAY, 16
-            def endDate = day.time
-            new CalendarEvent(
-                    userID: 1,
-                    name: "name #$it",
-                    description: "description #$it",
-                    location: "location #$it",
-                    eventType: CalendarEventType.findByName(eventTypes[it % 4]),
-                    startDate: startDate,
-                    endDate: endDate
-            ).save(failOnError: true)
-
-            day.roll Calendar.DAY_OF_YEAR, 1
-        }
+    entityHelperService.createEntityWithUserAndProfile("mike", etUser, "mpk@lernardo.at", "Mike P. Kuhl") {Entity ent ->
+      ent.profile.tagline = "Wozu brauch ma des?"
+      ent.profile.gender = 1
     }
-
-    void initUeDomains () {
-        // init metadata if DB is empty
-        defaultObjectService.onEmptyDatabase {
-            log.info "initializing empty database"
-            defaultObjectService.makeMetaData()
-
-            //create lernardo specific parametrisation
-            EntitySuperType estPerson = EntitySuperType.findByName (defaultObjectService.EST_PERSON)
-            EntityType etLma = new EntityType (name:"mitarbeiter")
-            estPerson.addToEntityTypes (etLma)
-            if (!estPerson.save()) {
-                estPerson.errors.each { log.error ("Person supertype: Bootstrap Error: $it") }
-                throw new IllegalArgumentException("failed to bootstrap entity structure metadata")
-            }
-
-            def mpk = entityHelperService.createEntityWithUserAndProfile("mike", etLma, "mpk@lernardo.at", "Mike P. Kuhl") {Entity ent->
-                def admin = Role.findByAuthority("ROLE_ADMIN")
-                assert admin
-                ent.user.addToAuthorities (admin)
-                ent.profile.tagline = "Wozu brauch ma des?"
-                ent.profile.gender = 1
-            }
-            def jlz = entityHelperService.createEntityWithUserAndProfile("johannes", etLma, "jlz@lernardo.at", "Johannes L. Zeitelberger") {Entity ent->
-                def admin = Role.findByAuthority("ROLE_ADMIN")
-                assert admin
-                ent.user.addToAuthorities (admin)
-                ent.profile.tagline = "Ich will die Welt im ERP abbilden!"
-                ent.profile.gender = 1
-            }
-            def aaz = entityHelperService.createEntityWithUserAndProfile("alexander", etLma, "aaz@lernardo.at", "Alexander Zeillinger") {Entity ent->
-                def admin = Role.findByAuthority("ROLE_ADMIN")
-                assert admin
-                ent.user.addToAuthorities (admin)
-                ent.profile.tagline = "Simplicity is the ultimate sophistication"
-                ent.profile.gender = 1
-            }
-            def pcr = entityHelperService.createEntityWithUserAndProfile("patrizia", etLma, "pcr@lernardo.at", "Patrizia Rosenkranz") {Entity ent->
-                def admin = Role.findByAuthority("ROLE_ADMIN")
-                assert admin
-                ent.user.addToAuthorities (admin)
-                ent.profile.tagline = "Auch Gott hat kein ERP"
-                ent.profile.gender = 2
-            }
-            def sst = entityHelperService.createEntityWithUserAndProfile("susanne", etLma, "sst@lernardo.at", "Susanne Stiedl") {Entity ent->
-                def admin = Role.findByAuthority("ROLE_ADMIN")
-                assert admin
-                ent.user.addToAuthorities (admin)
-                ent.profile.tagline = "..."
-                ent.profile.gender = 2
-            }
-            def bib = entityHelperService.createEntityWithUserAndProfile("birgit", etLma, "bib@lernardo.at", "Birgit Blaesen") {Entity ent->
-                def admin = Role.findByAuthority("ROLE_ADMIN")
-                assert admin
-                ent.user.addToAuthorities (admin)
-                ent.profile.tagline = "..."
-                ent.profile.gender = 2
-            }
-            def hmb = entityHelperService.createEntityWithUserAndProfile("hannah", etLma, "hmb@lernardo.at", "Hannah Mutzbauer") {Entity ent->
-                def admin = Role.findByAuthority("ROLE_ADMIN")
-                assert admin
-                ent.user.addToAuthorities (admin)
-                ent.profile.tagline = "..."
-                ent.profile.gender = 2
-            }
-            def rgt = entityHelperService.createEntityWithUserAndProfile("regina", etLma, "rgt@lernardo.at", "Regina Toncourt") {Entity ent->
-                def admin = Role.findByAuthority("ROLE_ADMIN")
-                assert admin
-                ent.user.addToAuthorities (admin)
-                ent.profile.tagline = "..."
-                ent.profile.gender = 2
-            }
-        }
-    } // initUeDomains
-} 
+    entityHelperService.createEntityWithUserAndProfile("johannes", etUser, "jlz@lernardo.at", "Johannes L. Zeitelberger") {Entity ent ->
+      ent.profile.tagline = "Ich will die Welt im ERP abbilden!"
+      ent.profile.gender = 1
+    }
+    entityHelperService.createEntityWithUserAndProfile("susanne", etUser, "sst@lernardo.at", "Susanne Stiedl") {Entity ent ->
+      ent.profile.tagline = "..."
+      ent.profile.gender = 2
+    }
+    entityHelperService.createEntityWithUserAndProfile("birgit", etUser, "bib@lernardo.at", "Birgit Blaesen") {Entity ent ->
+      ent.profile.tagline = "..."
+      ent.profile.gender = 2
+    }
+    entityHelperService.createEntityWithUserAndProfile("hannah", etUser, "hmb@lernardo.at", "Hannah Mutzbauer") {Entity ent ->
+      ent.profile.tagline = "..."
+      ent.profile.gender = 2
+    }
+    entityHelperService.createEntityWithUserAndProfile("regina", etUser, "rgt@lernardo.at", "Regina Toncourt") {Entity ent ->
+      ent.profile.tagline = "..."
+      ent.profile.gender = 2
+    }
+  }
+}
---
+++ grails-app/services/MetaDataService.groovy
@@ -0,0 +1,64 @@
+class MetaDataService {
+
+    boolean transactional = true
+
+    def defaultObjectService
+
+  // String Constants
+  static final String EST_PERSON = "Person"              // EntitySuperType
+  static final String EST_ORG = "Organisation"
+
+  static final String ET_USER = "User"                   // EntityType
+  static final String ET_PO = "ForProfitOrganisation"
+  static final String ET_NPO = "NonProfitOrganisation"
+
+  static final String PRT_PERSON = "Person"
+  static final String PRT_ORG = "Org"
+
+  static final String ROLE_USER = "ROLE_USER"
+  static final String ROLE_MOD = "ROLE_MOD"
+  static final String ROLE_ADMIN = "ROLE_ADMIN"
+
+  static final String LST_PRS = "PRS"                    // LinkSuperType (personal or organizational)
+  static final String LST_OA = "OA"
+
+  static final String LT_FRIEND = "Friend"               // LinkType
+  static final String LT_EMPLOYMENT = "Employment"
+
+  def initialize() {
+    getEstPerson()
+    getEstOrg()
+
+    getEtUser()
+    getEtPO()
+    getEtNPO()
+
+    getLstPRS()
+    getLstOA()
+
+    getLtFriend()
+    getLtEmployment()
+
+    getUserRole()
+    getModRole()
+    getAdminRole()
+  }
+
+  def getEstPerson() { defaultObjectService.openEST(EST_PERSON, PRT_PERSON)  }
+  def getEstOrg() { defaultObjectService.openEST(EST_ORG, PRT_ORG)  }
+
+  def getEtUser () { defaultObjectService.openET (ET_USER, estPerson) }
+  def getEtPO () { defaultObjectService.openET (ET_PO, estOrg) }
+  def getEtNPO () { defaultObjectService.openET (ET_NPO, estOrg) }
+
+  def getLstPRS () {defaultObjectService.openLST (LST_PRS, "Personal Relationship") }
+  def getLstOA () {defaultObjectService.openLST (LST_OA, "Organisational Association") }
+
+  def getLtFriend () { defaultObjectService.openLT(LT_FRIEND, lstPRS) }
+  def getLtEmployment () { defaultObjectService.openLT(LT_EMPLOYMENT, lstOA) }
+
+  def getUserRole () {defaultObjectService.openRole (ROLE_USER, "regular user")}
+  def getModRole () {defaultObjectService.openRole (ROLE_MOD, "moderator")}
+  def getAdminRole () {defaultObjectService.openRole (ROLE_ADMIN, "system administrator")}
+
+}
