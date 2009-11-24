f0b748cfd25b4295d0e5beeaa5532e8d48ab6706
Alexander Zeillinger <aaz@lkult.at>
2009-11-24 20:23:27 +0100

    Added FCKeditor plugin

---
+++ web-app/js/fck-config.js
@@ -0,0 +1,30 @@
+FCKConfig.ToolbarSets["Medium"] = [
+	['Save','Preview','-','Templates'],
+	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+	['Undo','Redo','-','SelectAll','RemoveFormat'],
+	'/',
+	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+	['OrderedList','UnorderedList','-','Outdent','Indent'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['Link','Unlink','Anchor'],
+	['Image','Flash','Table','Rule','Smiley','SpecialChar'],
+    ['EmbedMovies','YouTube'],
+    '/',
+	['Style','FontFormat','FontName','FontSize'],
+	['TextColor','BGColor'],
+	['FitWindow','ShowBlocks']		// No comma for the last row.
+] ;
+
+FCKConfig.ToolbarSets["Post"] = [
+	['Templates'],
+	['Cut','Copy','Paste','PasteText','PasteWord'],
+	['Undo','Redo','-','SelectAll','RemoveFormat'],
+	['Bold','Italic','Underline','StrikeThrough'],
+	['OrderedList','UnorderedList','-','Outdent','Indent'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+    '/',
+    ['Style','FontFormat'],
+	['Link','Unlink'],
+	['Image','Table','Rule','SpecialChar', 'YouTube'],
+	['FitWindow','ShowBlocks'/*,'Source'*/]		// No comma for the last row.
+] ;
