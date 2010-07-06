1897ac7ccfccbf5e7aa92fdfb5b9a6e2c9841b8a
Alexander Zeillinger <aaz@lkult.at>
2010-07-06 08:29:49 +0200

    update urlmappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,24 +1,26 @@
 class UrlMappings {
     static mappings = {
-      "/prf/$name/$content?"{
-          controller = 'profile'
-          action     = 'show'
-	      constraints {
-			 // apply constraints here
-		  }
-	  }
 
-      "/$controller/$action?/$id?"{
-	      constraints {
+      "/prf/$name/$content?"{
+        controller = 'profile'
+        action     = 'show'
+	     constraints {
 			 // apply constraints here
 		  }
-	  }
+	   }
+
+      // default mapping
+      "/$controller/$action?/$id?"{
+	     constraints {
+		    // apply constraints here
+		  }
+	   }
 
       "/" (controller:'app', action:'home') // public start
       "/start" (controller:'app', action:'start') // private start
 
-      //"404"(view:'404')
-      //"500"(view:'500')
+      "404"(view:'/404')
+      "500"(view:'/500')
       //"500"(view:'/error')
 	}
 }
