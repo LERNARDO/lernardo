59a842409e796034c91492f611cd3f66b8b8afdc
Alexander Zeillinger <aaz@uenterprise.de>
2011-04-08 07:36:42 +0200

    simplified project config

--- app-config/lernardo-config.groovy
+++ app-config/lernardo-config.groovy
@@ -1,168 +1,240 @@
-// ---------------------------------------------------------------------------------------------
-// database configuration
-// ---------------------------------------------------------------------------------------------
+// only change the project name here:
 
-dataSource {
-	pooled = false
-	driverClassName = "org.postgresql.Driver"
-    dialect = org.hibernate.dialect.PostgreSQLDialect
-	username = "sa_lernardo"
-	password = "pw_lernardo"
-}
-
-environments {
-	development {
-		dataSource {
-			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:postgresql://localhost:5432/lernardo"
-            loggingSql = false
-            hibernate.default_schema = 'dev'
-            grails.serverURL = "http://localhost:8080/lernardo"
-		}
-	}
-	test {
-		dataSource {
-          dbCreate = "update"
-          url = "jdbc:postgresql://localhost:5432/lernardo"
-          loggingSql = false
-          hibernate.default_schema = 'test'
-          grails.serverURL = "https://lernardo.customer.uenterprise.de/test"
-		}
-	}
-	production {
-		dataSource {
-          dbCreate = "update"
-          url = "jdbc:postgresql://localhost:5432/lernardo"
-          loggingSql = false
-          hibernate.default_schema = 'prod'
-          grails.serverURL = "http://sueninos.lernardo.net" // sueninos
-          //grails.serverURL = "http://noe.lernardo.net" // lernardo
-		}
-	}
-}
-
-// ---------------------------------------------------------------------------------------------
-// email configuration
-// ---------------------------------------------------------------------------------------------
-
-// this will be used if no "from" is supplied in a mail
-grails.mail.default.from="lernardomailer@gmail.com"
-
-// ---------------------------------------------------------------------------------------------
-// layout configuration
-// ---------------------------------------------------------------------------------------------
-
-// Sueninos Config:
+// possible values:
+// "sueninos"
+// "noe"
 
 project = "sueninos"
-projectName = "Sueninos" // full project name
-currency = "Pesos"
-timeZone = "America/Guatemala" // UTC-6
 
-// Lernardo Config:
+// nothing needs to be changed below this line
 
-/*project = "noe"
-projectName = "Lernardo"
-currency = "Euro"
-timeZone = "Europe/Vienna"*/ // UTC+1*/
+if (project == "sueninos") {
 
-// ---------------------------------------------------------------------------------------------
-// google analytics tracker
-// ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    // database configuration
+    // ---------------------------------------------------------------------------------------------
 
-// Sueninos Config:
+    dataSource {
+      pooled = false
+      driverClassName = "org.postgresql.Driver"
+      dialect = org.hibernate.dialect.PostgreSQLDialect
+      username = "sa_lernardo"
+      password = "pw_lernardo"
+    }
 
-google.analytics.webPropertyID = "UA-17725364-2" // http://sueninos.lernardo.net
+    environments {
+      development {
+        dataSource {
+              dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+              url = "jdbc:postgresql://localhost:5432/lernardo"
+              loggingSql = false
+              hibernate.default_schema = 'dev'
+              grails.serverURL = "http://localhost:8080/lernardo"
+        }
+      }
+      test {
+        dataSource {
+              dbCreate = "update"
+              url = "jdbc:postgresql://localhost:5432/lernardo"
+              loggingSql = false
+              hibernate.default_schema = 'test'
+              grails.serverURL = "https://lernardo.customer.uenterprise.de/test"
+        }
+      }
+      production {
+        dataSource {
+              dbCreate = "update"
+              url = "jdbc:postgresql://localhost:5432/lernardo"
+              loggingSql = false
+              hibernate.default_schema = 'prod'
+              grails.serverURL = "http://sueninos.lernardo.net"
+        }
+      }
+    }
 
-// Lernardo Config:
+    // ---------------------------------------------------------------------------------------------
+    // email configuration
+    // ---------------------------------------------------------------------------------------------
 
-//google.analytics.webPropertyID = "UA-17725364-3" // http://noe.lernardo.net
+    // this will be used if no "from" is supplied in a mail
+    grails.mail.default.from="lernardomailer@gmail.com"
 
-// by default, tracking is enabled for the 'production' Env only, but that can be overwritten
-// both ways (i.e emergency disable tracking)
-// google.analytics.enabled = false
+    // ---------------------------------------------------------------------------------------------
+    // layout configuration
+    // ---------------------------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------------------------
-// attribute visibility
-// ---------------------------------------------------------------------------------------------
+    projectName = "Sueninos" // full project name
+    currency = "Pesos"
+    timeZone = "America/Guatemala" // UTC-6
 
-// controls the visibility of attributes for each project
+    // ---------------------------------------------------------------------------------------------
+    // google analytics tracker
+    // ---------------------------------------------------------------------------------------------
 
-// Lernardo Config:
+    google.analytics.webPropertyID = "UA-17725364-2" // http://sueninos.lernardo.net
 
-// ClientProfile
-/*clientProfile.citizenship = true // true for Lernardo, false for Sueninos
-clientProfile.socialSecurityNumber = true // true for Lernardo, false for Sueninos
-clientProfile.contact = true // true for Lernardo, false for Sueninos
-clientProfile.job = false // false for Lernardo, true for Sueninos
-clientProfile.originZip = false // false for Lernardo, true for Sueninos
-clientProfile.originCity = false // false for Lernardo, true for Sueninos
-clientProfile.familyStatus = false // false for Lernardo, true for Sueninos
+    // by default, tracking is enabled for the 'production' Env only, but that can be overwritten
+    // both ways (i.e emergency disable tracking)
+    // google.analytics.enabled = false
 
-// GroupFamilyProfile
-groupFamilyProfile.familyIncome = false // false for Lernardo, true for Sueninos
-groupFamilyProfile.familyProblems = false // false for Lernardo, true for Sueninos
-groupFamilyProfile.amountHousehold = false // false for Lernardo, true for Sueninos
-groupFamilyProfile.city = true // true for Lernardo, false for Sueninos
+    // ---------------------------------------------------------------------------------------------
+    // attribute visibility
+    // ---------------------------------------------------------------------------------------------
 
-// ParentProfile
-parentProfile.socialSecurityNumber = true // true for Lernardo, false for Sueninos
-parentProfile.phone = true // true for Lernardo, false for Sueninos
-parentProfile.jobIncome = false // false for Lernardo, true for Sueninos
-parentProfile.jobFrequency = false // false for Lernardo, true for Sueninos
-parentProfile.education = false // false for Lernardo, true for Sueninos
-parentProfile.currentCountry = false // false for Lernardo, true for Sueninos
-parentProfile.citizenship = true // true for Lernardo, false for Sueninos
+    // controls the visibility of attributes
 
-// EducatorProfile
-educatorProfile.enlisted = false // false for Lernardo, true for Sueninos
-educatorProfile.origin = false // false for Lernardo, true for Sueninos
-educatorProfile.contact = false // false for Lernardo, true for Sueninos
-educatorProfile.phone = true // true for Lernardo, false for Sueninos
+    // ClientProfile
+    clientProfile.citizenship = false
+    clientProfile.socialSecurityNumber = false
+    clientProfile.contact = false
+    clientProfile.job = true
+    clientProfile.originZip = true
+    clientProfile.originCity = true
+    clientProfile.familyStatus = true
 
-jobs = ['unknown','employed','seekingwork','selfemployed','maternity','house']
-languages = ['spanish','german','english','french','italian','turkish','serbocroatian','russian','polish','bulgarian']
-educations = ['elementarypedagogue','universitypedagogue','specialpedagogue','nurserypedagogue','socialpedagogue','instrumentalpedagogue','secondarypedagogue','daynanny','childcare','hoardeducator','sociologist','psychologist','other']
-employments = ['employed','autonomous','trainee']
-schoollevels = ['first','second','third','fourth','fifth','sixth','seventh','eigth','ninth','tenth','eleventh','twelfth']
-inchargeof = ['hoardlead','hoardhelper','support','comeducator','externaleducator']*/
+    // GroupFamilyProfile
+    groupFamilyProfile.familyIncome = true
+    groupFamilyProfile.familyProblems = true
+    groupFamilyProfile.amountHousehold = true
+    groupFamilyProfile.city = false
 
-//Sueninos Config:
+    // ParentProfile
+    parentProfile.socialSecurityNumber = false
+    parentProfile.phone = false
+    parentProfile.jobIncome = true
+    parentProfile.jobFrequency = true
+    parentProfile.education = true
+    parentProfile.currentCountry = true
+    parentProfile.citizenship = false
 
-// ClientProfile
-clientProfile.citizenship = false // true for Lernardo, false for Sueninos
-clientProfile.socialSecurityNumber = false // true for Lernardo, false for Sueninos
-clientProfile.contact = false // true for Lernardo, false for Sueninos
-clientProfile.job = true // false for Lernardo, true for Sueninos
-clientProfile.originZip = true // false for Lernardo, true for Sueninos
-clientProfile.originCity = true // false for Lernardo, true for Sueninos
-clientProfile.familyStatus = true // false for Lernardo, true for Sueninos
+    // EducatorProfile
+    educatorProfile.enlisted = true
+    educatorProfile.origin = true
+    educatorProfile.contact = true
+    educatorProfile.phone = false
 
-// GroupFamilyProfile
-groupFamilyProfile.familyIncome = true // false for Lernardo, true for Sueninos
-groupFamilyProfile.familyProblems = true // false for Lernardo, true for Sueninos
-groupFamilyProfile.amountHousehold = true // false for Lernardo, true for Sueninos
-groupFamilyProfile.city = false // true for Lernardo, false for Sueninos
+    languages = ['spanish','german','english','french','mexican','portuguese','tsotsil','tseltal','zoque','tojolabal','kanjobal','lacandon','quiche','chol','cakchiquel','zapoteco']
+    educations = ['pedagogue','psychologist','sociologist','teacher','educator','psychopedagogue','artist','doctor','nurse','housekeeper','accountant']
+    employments = ['employed','freelancer','volunteer']
+    jobs = ['shoeblack','bugglegumseller','handcraftseller','builderslabourer','homework','childoverseer','carwasher','dispenser','bearer','garagehelper','handcrafter','garbagecollector','squeegeeman','farecollector']
+    schoollevels = ['firstnursery','secondnursery','thirdnursery','firstprimary','secondprimary','thirdprimary','fourthprimary','fifthprimary','sixthprimary','firstsecondary','secondsecondary','thirdsecondary','firstpreparatoria','secondpreparatoria','thirdpreparatoria','studies']
+    inchargeof = ['directorate','programcoordination','program','project','areas','tutor','cook','volunteer']
 
-// ParentProfile
-parentProfile.socialSecurityNumber = false // true for Lernardo, false for Sueninos
-parentProfile.phone = false // true for Lernardo, false for Sueninos
-parentProfile.jobIncome = true // false for Lernardo, true for Sueninos
-parentProfile.jobFrequency = true // false for Lernardo, true for Sueninos
-parentProfile.education = true // false for Lernardo, true for Sueninos
-parentProfile.currentCountry = true // false for Lernardo, true for Sueninos
-parentProfile.citizenship = false // true for Lernardo, false for Sueninos
+}
 
-// EducatorProfile
-educatorProfile.enlisted = true // false for Lernardo, true for Sueninos
-educatorProfile.origin = true // false for Lernardo, true for Sueninos
-educatorProfile.contact = true // false for Lernardo, true for Sueninos
-educatorProfile.phone = false // true for Lernardo, false for Sueninos
+if (project == "noe") {
 
-languages = ['spanish','german','english','french','mexican','portuguese','tsotsil','tseltal','zoque','tojolabal','kanjobal','lacandon','quiche','chol','cakchiquel','zapoteco']
-educations = ['pedagogue','psychologist','sociologist','teacher','educator','psychopedagogue','artist','doctor','nurse','housekeeper','accountant']
-employments = ['employed','freelancer','volunteer']
-jobs = ['shoeblack','bugglegumseller','handcraftseller','builderslabourer','homework','childoverseer','carwasher','dispenser','bearer','garagehelper','handcrafter','garbagecollector','squeegeeman','farecollector']
-schoollevels = ['firstnursery','secondnursery','thirdnursery','firstprimary','secondprimary','thirdprimary','fourthprimary','fifthprimary','sixthprimary','firstsecondary','secondsecondary','thirdsecondary','firstpreparatoria','secondpreparatoria','thirdpreparatoria','studies']
-inchargeof = ['directorate','programcoordination','program','project','areas','tutor','cook','volunteer']
+    // ---------------------------------------------------------------------------------------------
+    // database configuration
+    // ---------------------------------------------------------------------------------------------
+
+    dataSource {
+      pooled = false
+      driverClassName = "org.postgresql.Driver"
+        dialect = org.hibernate.dialect.PostgreSQLDialect
+      username = "sa_lernardo"
+      password = "pw_lernardo"
+    }
+
+    environments {
+      development {
+        dataSource {
+              dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+              url = "jdbc:postgresql://localhost:5432/lernardo"
+              loggingSql = false
+              hibernate.default_schema = 'dev'
+              grails.serverURL = "http://localhost:8080/lernardo"
+        }
+      }
+      test {
+        dataSource {
+              dbCreate = "update"
+              url = "jdbc:postgresql://localhost:5432/lernardo"
+              loggingSql = false
+              hibernate.default_schema = 'test'
+              grails.serverURL = "https://lernardo.customer.uenterprise.de/test"
+        }
+      }
+      production {
+        dataSource {
+              dbCreate = "update"
+              url = "jdbc:postgresql://localhost:5432/lernardo"
+              loggingSql = false
+              hibernate.default_schema = 'prod'
+              grails.serverURL = "http://noe.lernardo.net"
+        }
+      }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // email configuration
+    // ---------------------------------------------------------------------------------------------
+
+    // this will be used if no "from" is supplied in a mail
+    grails.mail.default.from="lernardomailer@gmail.com"
+
+    // ---------------------------------------------------------------------------------------------
+    // layout configuration
+    // ---------------------------------------------------------------------------------------------
+
+    projectName = "Lernardo"
+    currency = "Euro"
+    timeZone = "Europe/Vienna" // UTC+1
+
+    // ---------------------------------------------------------------------------------------------
+    // google analytics tracker
+    // ---------------------------------------------------------------------------------------------
+
+    google.analytics.webPropertyID = "UA-17725364-3" // http://noe.lernardo.net
+
+    // by default, tracking is enabled for the 'production' Env only, but that can be overwritten
+    // both ways (i.e emergency disable tracking)
+    // google.analytics.enabled = false
+
+    // ---------------------------------------------------------------------------------------------
+    // attribute visibility
+    // ---------------------------------------------------------------------------------------------
+
+    // controls the visibility of attributes
+
+    // ClientProfile
+    clientProfile.citizenship = true
+    clientProfile.socialSecurityNumber = true
+    clientProfile.contact = true
+    clientProfile.job = false
+    clientProfile.originZip = false
+    clientProfile.originCity = false
+    clientProfile.familyStatus = false
+
+    // GroupFamilyProfile
+    groupFamilyProfile.familyIncome = false
+    groupFamilyProfile.familyProblems = false
+    groupFamilyProfile.amountHousehold = false
+    groupFamilyProfile.city = true
+
+    // ParentProfile
+    parentProfile.socialSecurityNumber = true
+    parentProfile.phone = true
+    parentProfile.jobIncome = false
+    parentProfile.jobFrequency = false
+    parentProfile.education = false
+    parentProfile.currentCountry = false
+    parentProfile.citizenship = true
+
+    // EducatorProfile
+    educatorProfile.enlisted = false
+    educatorProfile.origin = false
+    educatorProfile.contact = false
+    educatorProfile.phone = true
+
+    jobs = ['unknown','employed','seekingwork','selfemployed','maternity','house']
+    languages = ['spanish','german','english','french','italian','turkish','serbocroatian','russian','polish','bulgarian']
+    educations = ['elementarypedagogue','universitypedagogue','specialpedagogue','nurserypedagogue','socialpedagogue','instrumentalpedagogue','secondarypedagogue','daynanny','childcare','hoardeducator','sociologist','psychologist','other']
+    employments = ['employed','autonomous','trainee']
+    schoollevels = ['first','second','third','fourth','fifth','sixth','seventh','eigth','ninth','tenth','eleventh','twelfth']
+    inchargeof = ['hoardlead','hoardhelper','support','comeducator','externaleducator']
+
+}
+
+
 
