e94fac1ce57f69ccbbe0039f4ecdc968e750fcc7
Rainer Oppel <rop@uenterprise.de>
2011-06-21 10:57:47 +0200

    another possible fix for ERP-677

--- grails-app/controllers/at/uenterprise/erp/SetupController.groovy
+++ grails-app/controllers/at/uenterprise/erp/SetupController.groovy
@@ -47,29 +47,29 @@ class SetupController {
   def removeElement = {
     def setupInstance = Setup.get(params.setupInstance)
     if (params.type == "bloodTypes")
-      setupInstance.removeFromBloodTypes(params.id.decodeHTML())
+      setupInstance.removeFromBloodTypes(params.element.decodeHTML())
     else if (params.type == "nationalities")
-      setupInstance.removeFromNationalities(params.id.decodeHTML())
+      setupInstance.removeFromNationalities(params.element.decodeHTML())
     else if (params.type == "languages")
-      setupInstance.removeFromLanguages(params.id.decodeHTML())
+      setupInstance.removeFromLanguages(params.element.decodeHTML())
     else if (params.type == "schoolLevels")
-      setupInstance.removeFromSchoolLevels(params.id.decodeHTML())
+      setupInstance.removeFromSchoolLevels(params.element.decodeHTML())
     else if (params.type == "workDescriptions")
-      setupInstance.removeFromWorkDescriptions(params.id.decodeHTML())
+      setupInstance.removeFromWorkDescriptions(params.element.decodeHTML())
     else if (params.type == "educations")
-      setupInstance.removeFromEducations(params.id.decodeHTML())
+      setupInstance.removeFromEducations(params.element.decodeHTML())
     else if (params.type == "employmentStatus")
-      setupInstance.removeFromEmploymentStatus(params.id.decodeHTML())
+      setupInstance.removeFromEmploymentStatus(params.element.decodeHTML())
     else if (params.type == "responsibilities")
-      setupInstance.removeFromResponsibilities(params.id.decodeHTML())
+      setupInstance.removeFromResponsibilities(params.element.decodeHTML())
     else if (params.type == "familyStatus")
-      setupInstance.removeFromFamilyStatus(params.id.decodeHTML())
+      setupInstance.removeFromFamilyStatus(params.element.decodeHTML())
     else if (params.type == "maritalStatus")
-      setupInstance.removeFromMaritalStatus(params.id.decodeHTML())
+      setupInstance.removeFromMaritalStatus(params.element.decodeHTML())
     else if (params.type == "partnerServices")
-      setupInstance.removeFromPartnerServices(params.id.decodeHTML())
+      setupInstance.removeFromPartnerServices(params.element.decodeHTML())
     else if (params.type == "familyProblems")
-      setupInstance.removeFromFamilyProblems(params.id.decodeHTML())
+      setupInstance.removeFromFamilyProblems(params.element.decodeHTML())
     render template: 'allElements', model: [setupInstance: setupInstance, type: params.type]
   }
 
@@ -96,8 +96,8 @@ class SetupController {
   def moveUp = {
     log.info 'params for moveUp:' + params
     def setupInstance = Setup.get(params.setupInstance)
-    if (setupInstance[params.type].indexOf(params.id.decodeHTML()) > 0) {
-      int i = setupInstance[params.type].indexOf(params.id.decodeHTML())
+    if (setupInstance[params.type].indexOf(params.element.decodeHTML()) > 0) {
+      int i = setupInstance[params.type].indexOf(params.element.decodeHTML())
       use(Collections){ setupInstance[params.type].swap(i, i - 1) }
     }
     render template: 'allElements', model: [setupInstance: setupInstance, type: params.type]
@@ -106,8 +106,8 @@ class SetupController {
   def moveDown = {
     log.info 'params for moveDown:' + params
     def setupInstance = Setup.get(params.setupInstance)
-    if (setupInstance[params.type].indexOf(params.id.decodeHTML()) < setupInstance[params.type].size() - 1) {
-      int i = setupInstance[params.type].indexOf(params.id.decodeHTML())
+    if (setupInstance[params.type].indexOf(params.element.decodeHTML()) < setupInstance[params.type].size() - 1) {
+      int i = setupInstance[params.type].indexOf(params.element.decodeHTML())
       use(Collections){ setupInstance[params.type].swap(i, i + 1) }
     }
     render template: 'allElements', model: [setupInstance: setupInstance, type: params.type]
