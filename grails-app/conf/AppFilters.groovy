400168ec995dbe06e9713609f8c3c41f8d5b3b06
Alexander Zeillinger <alexanderzeillinger@gmail.com>
2011-07-15 22:04:40 +0200

    simplified and documented filters

--- grails-app/conf/AppFilters.groovy
+++ grails-app/conf/AppFilters.groovy
@@ -6,6 +6,8 @@ class AppFilters {
 
   static filters = {
 
+    // adds the currently logged in user to the model passed to a GSP after an action has been executed
+    // doesn't work when rendering templates because a template is rendered directly to the response
     currentEntity(controller: "*", action: "*") {
       after = {model ->
         if (model) {
@@ -19,8 +21,11 @@ class AppFilters {
       }
     }
 
-    loginCheck(controller: "(groupActivityProfile|groupActivityTemplateProfile|groupClientProfile|groupColonyProfile|groupFamilyProfile|groupPartnerProfile|projectProfile|projectTemplateProfile|resourceProfile|themeProfile|childProfile|clientProfile|educatorProfile|facilityProfile|operatorProfile|parentProfile|partnerProfile|pateProfile|userProfile|activityProfile|msg|template)", action: "*") {
+    // checks if a user is logged in before executing an action
+    loginCheck(controller: "(app|articlePost|security)", invert: true) {
+    //loginCheck(controller: "(groupActivityProfile|groupActivityTemplateProfile|groupClientProfile|groupColonyProfile|groupFamilyProfile|groupPartnerProfile|projectProfile|projectTemplateProfile|resourceProfile|themeProfile|childProfile|clientProfile|educatorProfile|facilityProfile|operatorProfile|parentProfile|partnerProfile|pateProfile|userProfile|activityProfile|msg|template)", action: "*") {
       before = {
+        log.info "called controller: $controllerName, action: $actionName"
         Entity e = entityHelperService.getLoggedIn()
         if (!e) {
           redirect controller: 'app', action: 'home'
