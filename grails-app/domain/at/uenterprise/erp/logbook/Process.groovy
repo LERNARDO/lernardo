140fb36157a22703ae7a3bbf9975ce2c24b66f1b
Alexander Zeillinger <alexanderzeillinger@gmail.com>
2011-10-25 10:36:26 +0200

    Changes for ERP-866
    Some changes to the process class of the log book.

--- grails-app/domain/at/uenterprise/erp/logbook/Process.groovy
+++ grails-app/domain/at/uenterprise/erp/logbook/Process.groovy
@@ -4,11 +4,15 @@ import at.openfactory.ep.Entity
 
 class Process {
 
-  static hasMany = [facilities: Entity]
+  static hasMany = [facilities: Entity,
+                    types: String,
+                    entities: String]
 
   String  name
+  String  unit
   Integer costs = 0
 
   static constraints = {
+    unit nullable: true, inList: ['perDay','perMonth']
   }
 }
