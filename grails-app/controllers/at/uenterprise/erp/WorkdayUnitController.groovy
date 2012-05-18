44692cc845d028f235170682a917d4771408066f
Alexander Zeillinger <alexanderzeillinger@gmail.com>
2012-05-18 11:28:50 +0200

    Changes for ERP-881
    Show info when using broken IE with time records.

--- grails-app/controllers/at/uenterprise/erp/WorkdayUnitController.groovy
+++ grails-app/controllers/at/uenterprise/erp/WorkdayUnitController.groovy
@@ -111,74 +111,81 @@ class WorkdayUnitController {
     }
 
     def addWorkdayUnit = {
+      println params
       Entity entity = Entity.get(params.id)
 
-      WorkdayUnit workdayUnit = new WorkdayUnit(params)
+      // FIXME: workaround for IE not sending any formular data
+      if (params.date) {
+        WorkdayUnit workdayUnit = new WorkdayUnit(params)
 
-      Calendar calendar = Calendar.getInstance()
+        Calendar calendar = Calendar.getInstance()
 
-      // set start
-      Date from = params.date('from', 'HH:mm')
-      calendar.setTime(params.date)
-      calendar.set (Calendar.HOUR_OF_DAY, from.getHours())
-      calendar.set (Calendar.MINUTE, from.getMinutes())
-      workdayUnit.date1 = calendar.getTime()
-      workdayUnit.date1 = functionService.convertToUTC(workdayUnit.date1)
+        // set start
+        Date from = params.date('from', 'HH:mm')
+        calendar.setTime(params.date)
+        calendar.set (Calendar.HOUR_OF_DAY, from.getHours())
+        calendar.set (Calendar.MINUTE, from.getMinutes())
+        workdayUnit.date1 = calendar.getTime()
+        workdayUnit.date1 = functionService.convertToUTC(workdayUnit.date1)
 
-      // set end
-      Date to = params.date('to', 'HH:mm')
-      calendar.set (Calendar.HOUR_OF_DAY, to.getHours())
-      calendar.set (Calendar.MINUTE, to.getMinutes())
-      workdayUnit.date2 = calendar.getTime()
-      workdayUnit.date2 = functionService.convertToUTC(workdayUnit.date2)
+        // set end
+        Date to = params.date('to', 'HH:mm')
+        calendar.set (Calendar.HOUR_OF_DAY, to.getHours())
+        calendar.set (Calendar.MINUTE, to.getMinutes())
+        workdayUnit.date2 = calendar.getTime()
+        workdayUnit.date2 = functionService.convertToUTC(workdayUnit.date2)
 
-      // check if the end date is after the begin date
-      boolean datesOrdered = true
-      if (workdayUnit.date1 > workdayUnit.date2)
-        datesOrdered = false
+        // check if the end date is after the begin date
+        boolean datesOrdered = true
+        if (workdayUnit.date1 > workdayUnit.date2)
+          datesOrdered = false
 
-      // check if the to be created workday unit does intersect with an already existing workday unit
-      List existingWorkdayunits = []
-      if (entity.type.id == metaDataService.etEducator.id || entity.type.id == metaDataService.etUser.id) {
-        entity.profile.workdayunits.each { workday ->
-          if (workday.date1.getYear() == params.date.getYear() && workday.date1.getMonth() == params.date.getMonth() && workday.date1.getDate() == params.date.getDate()) {
-            existingWorkdayunits << workday
+        // check if the to be created workday unit does intersect with an already existing workday unit
+        List existingWorkdayunits = []
+        if (entity.type.id == metaDataService.etEducator.id || entity.type.id == metaDataService.etUser.id) {
+          entity.profile.workdayunits.each { workday ->
+            if (workday.date1.getYear() == params.date.getYear() && workday.date1.getMonth() == params.date.getMonth() && workday.date1.getDate() == params.date.getDate()) {
+              existingWorkdayunits << workday
+            }
           }
         }
-      }
 
-      boolean intersection = false
-      existingWorkdayunits.each { checked ->
-          if (!(workdayUnit.date1 >= checked.date2 || workdayUnit.date2 <= checked.date1)) {
-            // intersection, don't create
-            intersection = true
-          }
-      }
+        boolean intersection = false
+        existingWorkdayunits.each { checked ->
+            if (!(workdayUnit.date1 >= checked.date2 || workdayUnit.date2 <= checked.date1)) {
+              // intersection, don't create
+              intersection = true
+            }
+        }
 
-      if (!intersection && datesOrdered) {
-            // create it
-            workdayUnit.save(flush: true)
-            entity.profile.addToWorkdayunits(workdayUnit)
-      }
+        if (!intersection && datesOrdered) {
+              // create it
+              workdayUnit.save(flush: true)
+              entity.profile.addToWorkdayunits(workdayUnit)
+        }
 
-      List workdayunits = []
-      if (entity.type.id == metaDataService.etEducator.id || entity.type.id == metaDataService.etUser.id) {
-        entity.profile.workdayunits.each { workday ->
-          if (workday.date1.getYear() == params.date.getYear() && workday.date1.getMonth() == params.date.getMonth() && workday.date1.getDate() == params.date.getDate()) {
-            workdayunits << workday
+        List workdayunits = []
+        if (entity.type.id == metaDataService.etEducator.id || entity.type.id == metaDataService.etUser.id) {
+          entity.profile.workdayunits.each { workday ->
+            if (workday.date1.getYear() == params.date.getYear() && workday.date1.getMonth() == params.date.getMonth() && workday.date1.getDate() == params.date.getDate()) {
+              workdayunits << workday
+            }
           }
         }
+
+        List workdaycategories = WorkdayCategory.list()
+
+
+        render template: 'workdayunits', model: [workdayunits: workdayunits,
+                                                date: params.date,
+                                                workdaycategories: workdaycategories,
+                                                intersection: intersection,
+                                                datesOrdered: datesOrdered,
+                                                entity: entity,
+                                                currentEntity: entityHelperService.loggedIn]
       }
-
-      List workdaycategories = WorkdayCategory.list()
-
-      render template: 'workdayunits', model: [workdayunits: workdayunits,
-                                              date: params.date,
-                                              workdaycategories: workdaycategories,
-                                              intersection: intersection,
-                                              datesOrdered: datesOrdered,
-                                              entity: entity,
-                                              currentEntity: entityHelperService.loggedIn]
+      else
+        render '<p class="red">' + message(code:"iebuggy") + '</p>'
     }
 
     def confirmDays = {
