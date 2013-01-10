4bbfe50405fe7ae53bc1b9ea1e3cd61d88ac9d71
Alexander Zeillinger <alexanderzeillinger@gmail.com>
2013-01-10 12:21:22 +0100

    Use DSL to specify plugin dependencies.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -34,5 +34,27 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         // runtime 'mysql:mysql-connector-java:5.1.5'
     }
+    plugins {
+        compile ":cache-headers:1.1.5"
+        compile ":cached-resources:1.0"
+        compile ":ckeditor:3.6.3.0"
+        compile ":codenarc:0.17"
+        compile ":export:1.3"
+        compile ":google-analytics:1.0"
+        compile ":grails-melody:1.14"
+        compile ":hibernate:$grailsVersion"
+        compile ":jquery:1.7.1"
+        compile ":jquery-ui:1.8.15"
+        compile ":lesscss-resources:1.3.0.3"
+        compile ":mail:0.9"
+        compile ":profiler:0.4"
+        compile ":quartz:0.4.2"
+        compile ":remote-pagination:0.3"
+        compile ":rendering:0.4.2"
+        compile ":resources:1.1.6"
+        compile ":svn:1.0.0.M1"
+        compile ":tomcat:$grailsVersion"
+        compile ":zipped-resources:1.0"
+    }
 
 }
